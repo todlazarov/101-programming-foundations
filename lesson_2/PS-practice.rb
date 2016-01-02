6407643ddc395190e2175f9d96d9b8ab454c31ab
Tod Lazarov <tod.lazarov1@gmail.com>
2016-01-02 09:14:15 -0500

    Complete the pseudo-code practice exercise

---
+++ lesson_2/PS-practice.rb
@@ -0,0 +1,28 @@
+# Practice pseudo-code 
+# 1.A method that returns the sum of two integers
+# 2.A method that takes an array of strings, and returns a string that is all those strings concatenated together
+# 3.A method that takes an array of integers, and returns a new array with every other element
+
+# 1
+
+# Casual
+
+# Take the first number
+# Take the second number
+# Add them together
+# Puts the results
+
+
+
+# Formal
+
+# START
+
+# GET the first number
+# GET the second number
+
+# SET the results variable by adding the two numbers together
+
+# PRINT the result
+
+# END
