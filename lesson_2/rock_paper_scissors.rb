215b1c5be1133610944c8809af5a335a9bd33757
Tod Lazarov <tod.lazarov1@gmail.com>
2016-01-04 15:07:22 -0500

    Add input check

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,22 +1,24 @@
-CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors)
 
 def prompt(message)
-  puts ("=> #{message}")
+  puts("=> #{message}")
 end
 
 def display_results(player_choice, computer_choice)
-  if (player_choice == "rock" && computer_choice == "scissors") || 
-     (player_choice == "paper" && computer_choice == "rock") || 
+  if (player_choice == "rock" && computer_choice == "scissors") ||
+     (player_choice == "paper" && computer_choice == "rock") ||
      (player_choice == "scissors" && computer_choice == "paper")
     prompt("You win!")
-  elsif (player_choice == "rock" && computer_choice == "paper") || 
-     (player_choice == "paper" && computer_choice == "scissors") || 
-     (player_choice == "scissors" && computer_choice == "rock")
+  elsif (player_choice == "rock" && computer_choice == "paper") ||
+        (player_choice == "paper" && computer_choice == "scissors") ||
+        (player_choice == "scissors" && computer_choice == "rock")
     prompt("Computer wins!")
   else
     prompt("It's a tie!")
-  end  
+  end
 end
+
+player_choice = ''
 
 prompt("Welcome to our Rock, Paper, Scissors game!")
 
@@ -26,9 +28,16 @@ loop do
   prompt("2. Paper")
   prompt("3. Scissors")
 
-  player_choice = gets.chomp.downcase
+  loop do
+    player_choice = gets.chomp.downcase
+    if VALID_CHOICES.include? player_choice
+      break
+    else
+      prompt("Please enter a valid choice")
+    end
+  end
 
-  computer_choice = CHOICES.sample
+  computer_choice = VALID_CHOICES.sample
 
   prompt("You selected #{player_choice}.")
   prompt("The computer selecter #{computer_choice}.")
@@ -39,3 +48,5 @@ loop do
   repeat = gets.chomp.downcase
   break unless repeat.start_with?("y")
 end
+
+prompt("Thank you for playing! Goodbye")
