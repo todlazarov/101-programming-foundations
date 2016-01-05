687d44b9cff3c41b899e631442238e575f9b3350
Tod Lazarov <tod.lazarov1@gmail.com>
2016-01-05 18:34:55 -0500

    Add a score keep

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -5,6 +5,8 @@ VALID_ABRV = %w(r p sc l sp)
 # Variables
 choice = ''
 player_choice = ''
+$player_score = 0
+$computer_score = 0
 
 # Methods
 def prompt(message)
@@ -22,11 +24,18 @@ end
 def display_results(player_choice, computer_choice)
   if win?(player_choice, computer_choice)
     prompt("You win!")
+    score_tracker("player")
   elsif win?(computer_choice, player_choice)
     prompt("Computer wins!")
+    score_tracker("computer")
   else
     prompt("It's a tie!")
   end
+end
+
+def score_tracker(winner)
+  $player_score += 1 if winner == "player"
+  $computer_score += 1 if winner == "computer"
 end
 
 def abrv_to_full_word(word)
@@ -44,7 +53,14 @@ def abrv_to_full_word(word)
   end
 end
 
+def display_score(score_1, score_2)
+  prompt("The score is...")
+  prompt("You: #{score_1}")
+  prompt("Computer: #{score_2}")
+end
+
 prompt("Welcome to our Rock, Paper, Scissors, Lizard, Spock game!")
+prompt("First player to 5 points wins the game!")
 
 loop do
   prompt("Choose an option: #{VALID_CHOICES.join(', ')}")
@@ -67,9 +83,20 @@ loop do
 
   display_results(player_choice, computer_choice)
 
-  prompt("Would you like to play again? (Y for yes)")
-  repeat = gets.chomp.downcase
-  break unless repeat.start_with?("y")
+  if $player_score == 5
+    prompt("Congratulations! You defeated the computer!")
+    prompt("Would you like to beat his a$$ again?(type y for Yes)")
+    repeat = gets.chomp.downcase
+    break unless repeat.start_with?("y")
+  elsif $computer_score == 5
+    prompt("You were beaten by the computer! You suck!")
+    prompt("How about you try again and do us humans proud?(type y for Yes)")
+    repeat = gets.chomp.downcase
+    break unless repeat.start_with?("y")
+  end
+  
+  display_score($player_score, $computer_score)
+  prompt("------------------------")
 end
 
 prompt("Thank you for playing! Goodbye")
