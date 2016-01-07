a7e03ae1c383df71a40bfbd13af549763c1b1287
Tod Lazarov <tod.lazarov1@gmail.com>
2016-01-07 17:34:57 -0500

    Fix score keeping errors and finalize

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -3,100 +3,115 @@ VALID_CHOICES = %w(rock paper scissors lizard spock)
 VALID_ABRV = %w(r p sc l sp)
 
 # Variables
+score = [0, 0]
 choice = ''
-player_choice = ''
-$player_score = 0
-$computer_score = 0
 
 # Methods
-def prompt(message)
-  puts("=> #{message}")
+def prompt(string)
+  puts "=> #{string}"
 end
 
 def win?(first, second)
-  (first == "rock" && (second == "scissors" || second == "lizard")) ||
-    (first == "paper" && (second == "rock" || second == "spock")) ||
-    (first == "scissors" && (second == "paper" || second == "lizard")) ||
-    (first == "lizard" && (second == "spock" || second == "paper")) ||
-    (first == "spock" && (second == "scissors" || second == "rock"))
+  first == "rock" && (second == "scissors" || second == "lizard") ||
+    first == "paper" && (second == "rock" || second == "spock") ||
+    first == "scissors" && (second == "paper" || second == "lizard") ||
+    first == "lizard" && (second == "paper" || second == "spock") ||
+    first == "spock" && (second == "scissors" || second == "rock")
 end
 
-def display_results(player_choice, computer_choice)
-  if win?(player_choice, computer_choice)
+def results(array, choice_1, choice_2)
+  if win?(choice_1, choice_2)
     prompt("You win!")
-    score_tracker("player")
-  elsif win?(computer_choice, player_choice)
+    score_tracker(array, 0)
+  elsif win?(choice_2, choice_1)
     prompt("Computer wins!")
-    score_tracker("computer")
+    score_tracker(array, 1)
   else
-    prompt("It's a tie!")
+    puts "Its a tie!"
   end
 end
 
-def score_tracker(winner)
-  $player_score += 1 if winner == "player"
-  $computer_score += 1 if winner == "computer"
+def score_tracker(array,num)
+  if num == 0
+    array[0] += 1
+  elsif num == 1
+    array[1] += 1
+  end
+end
+
+def display(array)
+  prompt("The score is...")
+  prompt("You: #{array[0]}")
+  prompt("Computer: #{array[1]}")
 end
 
 def abrv_to_full_word(word)
   case word
   when "r"
-    player_choice = "rock"
+    word << "ock"
   when "p"
-    player_choice = "paper"
+    word << "aper"
   when "sc"
-    player_choice = "scissors"
+    word << "issors"
   when "l"
-    player_choice = "lizard"
+    word << "izard"
   when "sp"
-    player_choice = "spock"
-  end
+    word << "ock"
+  end 
 end
 
-def display_score(score_1, score_2)
-  prompt("The score is...")
-  prompt("You: #{score_1}")
-  prompt("Computer: #{score_2}")
-end
-
+# Main Logic
 prompt("Welcome to our Rock, Paper, Scissors, Lizard, Spock game!")
 prompt("First player to 5 points wins the game!")
 
 loop do
-  prompt("Choose an option: #{VALID_CHOICES.join(', ')}")
-  prompt("You can type in the first letter only(HINT: sc for scissors and sp for spock)")
+  prompt("Please make your choice #{VALID_CHOICES.join(', ')}")
+  prompt("You can type in the first letter only (HINT: sc for scissors and sp for spock")
 
+  # Input check
   loop do
     choice = gets.chomp.downcase
     if VALID_CHOICES.include?(choice) || VALID_ABRV.include?(choice)
       break
     else
-      prompt("Please enter a valid choice")
+      prompt("Plase enter a valid choice")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
-  player_choice = abrv_to_full_word(choice)
-
-  prompt("You selected #{player_choice}.")
-  prompt("The computer selecter #{computer_choice}.")
-
-  display_results(player_choice, computer_choice)
-
-  if $player_score == 5
-    prompt("Congratulations! You defeated the computer!")
-    prompt("Would you like to beat his a$$ again?(type y for Yes)")
-    repeat = gets.chomp.downcase
-    break unless repeat.start_with?("y")
-  elsif $computer_score == 5
-    prompt("You were beaten by the computer! You suck!")
-    prompt("How about you try again and do us humans proud?(type y for Yes)")
-    repeat = gets.chomp.downcase
-    break unless repeat.start_with?("y")
+  # Check in case the short version is used
+  if VALID_ABRV.include?(choice)  
+    player_choice = abrv_to_full_word(choice)
+  else
+    player_choice = choice
   end
-  
-  display_score($player_score, $computer_score)
-  prompt("------------------------")
+
+  # Random computer choice
+  computer_choice = VALID_CHOICES.sample
+
+  # Display of choices
+  prompt("You picked #{player_choice}")
+  prompt("Computer picked #{computer_choice}")
+
+  # Outcome calculator and display of results
+  results(score, player_choice, computer_choice)
+  display(score)
+
+  # Check if one of the players has won the game
+  if score[0] == 5
+    prompt("Congratulations! You defeated the computer!")
+    prompt("Would you like to defeat the machines again?(type y for yes)")
+    repeat = gets.chomp.downcase
+    break unless repeat.start_with?('y')
+    score = [0,0]
+  elsif score[1] == 5
+    prompt("You were defeated by the computer! You suck")
+    prompt("How about you try again and do us humans proud?(type y for yes)")
+    repeat = gets.chomp.downcase
+    break unless repeat.start_with?('y')
+    score = [0,0]
+  end
+
+  prompt("---------------------")
 end
 
-prompt("Thank you for playing! Goodbye")
+prompt("Thank you for playing. Goobye.")
