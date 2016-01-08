15eed491d2c4e8a423a4d6d27ec4a88d8024cb45
Tod Lazarov <tod.lazarov1@gmail.com>
2016-01-07 20:04:28 -0500

    Finish Easy 3 section

---
+++ lesson_3/Easy_3/Ex2.rb
@@ -0,0 +1,10 @@
+# How can we add the family pet "Dino" to our usual array:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones.push("Dino")
+
+puts flintstones
+
+# or
+# flinstones << "Dino"
