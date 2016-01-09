63d141af5143330e48eeaffb8238b820e44f7714
Tod Lazarov <tod.lazarov1@gmail.com>
2016-01-09 00:36:35 -0500

    Finish Ex9.rb

--- lesson_3/Medium_1/Ex9.rb
+++ lesson_3/Medium_1/Ex9.rb
@@ -10,16 +10,25 @@ munsters = {
 
 # Modify the hash such that each member of the Munster family has an additional "age_group" key that has one of three values describing the age group the family member is in (kid, adult, or senior). Your solution should produce the hash below
 
-{ "Herman" => { "age" => 32, "gender" => "male", "age_group" => "adult" },
-  "Lily" => {"age" => 30, "gender" => "female", "age_group" => "adult" },
-  "Grandpa" => { "age" => 402, "gender" => "male", "age_group" => "senior" },
-  "Eddie" => { "age" => 10, "gender" => "male", "age_group" => "kid" },
-  "Marilyn" => { "age" => 23, "gender" => "female", "age_group" => "adult" } }
+# { "Herman" => { "age" => 32, "gender" => "male", "age_group" => "adult" },
+#   "Lily" => {"age" => 30, "gender" => "female", "age_group" => "adult" },
+#   "Grandpa" => { "age" => 402, "gender" => "male", "age_group" => "senior" },
+#   "Eddie" => { "age" => 10, "gender" => "male", "age_group" => "kid" },
+#   "Marilyn" => { "age" => 23, "gender" => "female", "age_group" => "adult" } }
 
 # Note: a kid is in the age range 0 - 17, an adult is in the range 18 - 64 and a senior is aged 65+.
 
 # hint: try using a case statement along with Ruby Range objects in your solution
 
-munsters.map do |each| 
-  puts each 
+munsters.each do |k, v| 
+  case v["age"]
+  when 0..17
+    v["age_group"] = "kid"
+  when 18..64
+    v["age_group"] = "adult"
+  else
+    v["age_group"] = "senior" 
+  end
 end
+
+puts munsters
