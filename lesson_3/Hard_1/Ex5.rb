5da20914e168cb675926b1f1624fe852c300c023
Tod Lazarov <tod.lazarov1@gmail.com>
2016-01-11 06:26:11 -0500

    Compelte lesson 3

--- lesson_3/Hard_1/Ex5.rb
+++ lesson_3/Hard_1/Ex5.rb
@@ -4,11 +4,14 @@
 
 def dot_separated_ip_address?(input_string)
   dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.size == 4
+
   while dot_separated_words.size > 0 do
     word = dot_separated_words.pop
-    break if !is_a_number?(word)
+    return false unless is_a_number?(word)
   end
-  return true
+  
+  true
 end
 
 # Alyssa reviewed Ben's code and says "It's a good start, but you missed a few things. 
