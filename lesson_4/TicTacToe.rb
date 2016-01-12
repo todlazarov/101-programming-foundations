45b4551a3e53160e8d1fee94bc365a836a2f489f
Tod Lazarov <tod.lazarov1@gmail.com>
2016-01-11 23:02:19 -0500

    Add score and joinor methods

--- lesson_4/TicTacToe.rb
+++ lesson_4/TicTacToe.rb
@@ -1,4 +1,5 @@
-# Constants
+require 'pry'
+
 INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
@@ -6,14 +7,23 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
 
+# Variables
+score_arr = [0, 0]
+
 # Methods
 def prompt(msg)
   puts "=> #{msg}"
 end
 
-def display_board(brd)
+def joinor(arr, separator, word="or")
+  arr[-1] = "#{word} #{arr.last}" if arr.size > 1
+  arr.join(separator)
+end
+
+def display_board(brd, arr)
   system "clear"
   puts "You are #{PLAYER_MARKER}, the computer is #{COMPUTER_MARKER}"
+  puts "Current score: Player #{arr[0]}, Computer #{arr[1]}"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -42,7 +52,7 @@ end
 def player_turn(brd)
   choice = ''
   loop do
-    prompt "Pick a square (#{empty_squares(brd).join(', ')}):"
+    prompt "Pick a square #{joinor(empty_squares(brd), ', ')}"
     choice = gets.chomp.to_i
     break if empty_squares(brd).include?(choice)
     prompt "This is not a valid choice."
@@ -75,32 +85,38 @@ def winner?(brd)
   !!who_wins?(brd)
 end
 
+def score(winner, arr)
+  if winner == "Player"
+    arr[0] += 1
+  elsif winner == "Computer"
+    arr[1] += 1
+  end
+end
+
 # Main logic
 loop do
   board = initialize_game
 
   loop do
-    display_board(board)
+    display_board(board, score_arr)
 
     player_turn(board)
     break if winner?(board) || board_full?(board)
+
     computer_turn(board)
-
     break if winner?(board) || board_full?(board)
-    display_board(board)
   end
-
-  display_board(board)
 
   if winner?(board)
     prompt "#{who_wins?(board)} wins!"
   else
     prompt "Its a tie!"
   end
+  sleep(2)
+  score(who_wins?(board), score_arr)
 
-  prompt "Would you like to play again?(y or n)"
-  repeat = gets.chomp
-  break unless repeat.downcase.start_with?('y')
+  display_board(board, score_arr)
+  break if score_arr[0] == 5 or score_arr[1] == 5
 end
 
 prompt "Thank you for playing TicTacToe! Goodbye!"
